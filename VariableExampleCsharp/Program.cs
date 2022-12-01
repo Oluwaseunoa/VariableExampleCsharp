b13dad4761d82555221347fe7b288c6cbec5de9d
oluwaseunoa <oluwaseun.beicks@gmail.com>
2022-12-01 17:39:06 +0100

    Add project files.

---
+++ VariableExampleCsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿class Sample 
+{
+    static void Main()
+    {
+        //declaring a variable
+        string studentName = "Seun";
+        int age = 20;
+
+
+        //print value
+        System.Console.WriteLine("Hey ");
+        System.Console.WriteLine(studentName);
+        System.Console.WriteLine("Your age is ");
+        System.Console.WriteLine(age);
+
+        //wait for pressing some key
+        System.Console.ReadKey();
+    }
+
+}
